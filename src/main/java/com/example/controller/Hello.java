1196496114fa5eba8ce9b476997efcb9a055b50d
zyy <459478315@qq.com>
2020-07-30 08:45:40 +0800

    第一次提交

---
+++ src/main/java/com/example/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shkstart
+ * @create 2020-07-30 8:28
+ */
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+
+    }}
